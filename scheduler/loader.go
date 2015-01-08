7cc761c7754f1790543414c85a5b1135c8e31924
Andrew Matheny <andrew.j.matheny@gmail.com>
2015-01-08 17:44:39 -0500

    Don't cache the scheduler in case of problem connecting to the cluster

--- scheduler/loader.go
+++ scheduler/loader.go
@@ -5,12 +5,7 @@ import (
 	"github.com/listhub/please/scheduler/citadel"
 )
 
-var scheduler model.Scheduler
-
 // Get persistence from the environment config
 func Get() model.Scheduler {
-	if scheduler == nil {
-		scheduler = citadel.New()
-	}
-	return scheduler
+	return citadel.New()
 }
