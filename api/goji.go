f0327fdf8dfd58e4f681766c190640a03a379386
Shane Starcher <shanestarcher@gmail.com>
2015-01-08 19:09:09 -0500

    Add Cors

--- api/goji.go
+++ api/goji.go
@@ -3,11 +3,17 @@ package api
 import (
 	"github.com/listhub/please/api/v1/jobs"
 	"github.com/listhub/please/api/v1/status"
+	"github.com/rs/cors"
 	"github.com/zenazn/goji"
 )
 
 // ServeAPI ...
 func ServeAPI() {
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	})
+	goji.Use(c.Handler)
+
 	goji.Get("/api/v1/jobs", jobs.GetJobs)
 	goji.Post("/api/v1/jobs", jobs.NewJob)
 	goji.Delete("/api/v1/jobs/:job_name", jobs.DeleteJob)
